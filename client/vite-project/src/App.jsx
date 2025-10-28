e2c3350369f08dd6c65aaa5f6057b051f55e50f9
sayak roy <08sayakroy@gmail.com>
2025-10-28 17:53:39 +0530

    updated App.jsx

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
-import Home from './pages/home'
-import Login from "./pages/login";
-import EmailVerify from "./pages/emailVerify";
-import ResetPassword from "./pages/resetPassword";
+import Home from './pages/Home.jsx'
+import Login from "./pages/Login.jsx";
+import EmailVerify from "./pages/EmailVerify.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
 import { ToastContainer} from 'react-toastify';
 
 const App = () => {
