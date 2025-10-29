db9c84b762129a32b744a3b639190ff31bfa5645
sayak roy <08sayakroy@gmail.com>
2025-10-29 12:07:51 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const allowedOrigins = [
-    'http://localhost:5173',]
+    "http://localhost:5173",
+    "https://authentication-system-frontend-m7gmdw29y.vercel.app"]
 
 app.use(express.json());
 app.use(cookieParser());
